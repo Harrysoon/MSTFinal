2e7431ba1af7550a89eb34f5e9bba6eaa9bf99ac
Harrysoon <harry.fleetwood@gmail.com>
2016-07-08 13:08:56 +0100

    Updated a bit of the contact page

---
+++ MSTFinal/Models/ContactModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MSTFinal.Models
+{
+    public class ContactModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string MobileNumber { get; set; }
+
+        public string Message { get; set; }
+    }
+}
